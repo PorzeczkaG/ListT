7ce422ae6ec77c22c34477a22121d3f3c26ee343
PorzeczkaG <64199094+PorzeczkaG@users.noreply.github.com>
2020-04-26 11:11:17 +0200

    Dodaj pliki projektów.

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListT
+{
+    class osoby
+    {
+
+
+            public int id { get; set; }
+            public string name { get; set; }
+
+        
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<osoby> os = new List<osoby>();
+            Console.Write("Ile rekordow chcesz dodac do listy?  ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Wprowadz imie.   ");
+                osoby emp = new osoby();
+                emp.name = Console.ReadLine();
+                emp.id = i + 1;
+                os.Add(emp); ;
+                Console.WriteLine("Dodano rekord.");
+                Console.WriteLine("Nacisnij Enter aby kontynuowac.");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Nacisnij Enter aby wyswietlic liste.");
+            Console.ReadLine();
+
+            foreach (var item in os)
+            {
+                Console.WriteLine("ID:  " + item.id);
+                Console.WriteLine("Imie:    " + item.name);
+                Console.WriteLine("-------------------------------");
+            }
+            Console.ReadKey();
+        }
+    }
+}
